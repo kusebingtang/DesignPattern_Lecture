adcfa71de2dced153e8b464c0fb34f7bd24fbf82
jbw <jb@98game.cn>
2020-03-27 11:34:25 +0800

    init design pattern project

---
+++ src/main/java/org/lecture/design/pattern/Test.java
@@ -0,0 +1,12 @@
+package org.lecture.design.pattern;
+
+/**
+ * @author JiangBin
+ * @create 2020-03-27 11:06
+ */
+public class Test {
+
+    public static void main(String[] args) {
+        System.out.println("ni你好");
+    }
+}
