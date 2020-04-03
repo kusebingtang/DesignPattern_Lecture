7bb1978e580278d1293ed08b66a533c64cb5bbf6
jbw <jb@98game.cn>
2020-04-03 10:29:10 +0800

    简单工厂模式

---
+++ src/main/java/org/lecture/design/pattern/factory/simplefactory/pizzastore/pizza/GreekPizza.java
@@ -0,0 +1,10 @@
+package org.lecture.design.pattern.factory.simplefactory.pizzastore.pizza;
+
+public class GreekPizza extends Pizza {
+
+    @Override
+    public void prepare() {
+        System.out.println(" 给希腊披萨 准备原材料 ");
+    }
+
+}
